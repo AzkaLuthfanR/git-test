defef9e0cfd268300ed61ad4215488a8fc6e7353
AzkaLuthfanR <azkaluthfanr@gmail.com>
2022-01-15 11:35:28 +0700

    menambahkan 1 line println di file test.java

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test{
     public static void main(String[] args) {
         System.out.println("belajar GIT-Remote 01");
+        System.out.println("menambahkan 1 println");
     }
 }
